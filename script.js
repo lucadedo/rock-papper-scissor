a1cd363e3b1cba039acb5ebede329c07cbe37d02
luca <lucadedo99@icloud.com>
2022-09-26 15:20:39 +0800

    ADD PAPER AND SCISSORS CONDITIONS IN PLAYROUND()
    new coditions else if , when player choose paper or scissors

--- script.js
+++ script.js
@@ -14,7 +14,6 @@ function getComputerChoice(){
     return result;    
 } 
     
-getComputerChoice();
 
 function playRound(playerSelection,computerSelection){
     playerSelection = playerSelection.toUpperCase();
@@ -26,19 +25,52 @@ function playRound(playerSelection,computerSelection){
 
     }else if (playerSelection === 'ROCK' && computerSelection === 'ROCK'){
 
-        let result = "No one wins! Rock and Rock are tie!";
+        let result = "No one wins! Rock and Rock are a tie!";
         return result;
 
     }else if (playerSelection === 'ROCK' && computerSelection === 'SCISSORS'){
         
-        let result = "You won! Rock beats Scissors!"
+        let result = "You won! Rock beats Scissors!";
         return result;
+
+    }else if (playerSelection === 'PAPER' && computerSelection === 'PAPER'){
+
+        let result = "No one wins! Paper and Paper are a tie!";
+        return result;
+
+    }else if (playerSelection === 'PAPER' && computerSelection === 'ROCK'){
+
+        let result = "You won! Paper beats Rock!";
+        return result;
+
+    }else if (playerSelection === 'PAPER' && computerSelection === 'SCISSORS'){
+
+        let result = "You lose! Scissors beats Paper!";
+        return result;
+
+    }else if (playerSelection === 'SCISSORS' && computerSelection === 'PAPER'){
+
+        let result = "You won! Scissors beast Paper";
+        return result;
+
+    }else if (playerSelection === 'SCISSORS' && computerSelection === 'ROCK'){
+
+        let result = "You lose! Rock beats Scissors";
+        return result;
+
+    }else if(playerSelection === 'SCISSORS' && computerSelection === 'SCISSORS'){
+
+        let result = "No one wins! Scissors and Scissors are a tie!";
+        return result;
+
     }
+
+        
 
 
 }
 
-const playerSelection = 'rock';
+const playerSelection = prompt("enter your choice:");
 const computerSelection = getComputerChoice();
 console.log(playRound(playerSelection,computerSelection));
 
