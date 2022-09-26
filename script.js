db054b7ec4602ade63e23b7a1111ae70fb7cf3ee
luca <lucadedo99@icloud.com>
2022-09-26 14:10:45 +0800

    Create the playRound function
    only create the function for the player that choose rock
    and the function decides if you won or losed or tied,with if...else if
    conditions.

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 function getComputerChoice(){
     let result = Math.floor(Math.random() * 3);
+    console.log(result);
     if (result===0){
         result = 'Rock';
         
@@ -9,9 +10,35 @@ function getComputerChoice(){
         result = 'Scissors';
         }
     
-    console.log(result);
-    return result;
-    
+    console.log('computer choose: ',result);
+    return result;    
 } 
     
 getComputerChoice();
+
+function playRound(playerSelection,computerSelection){
+    playerSelection = playerSelection.toUpperCase();
+    computerSelection = computerSelection.toUpperCase();
+    console.log(playerSelection,computerSelection);
+    if (playerSelection === 'ROCK' && computerSelection === 'PAPER'){
+        let result = "You Lose! Paper beats Rock";
+        return result;
+
+    }else if (playerSelection === 'ROCK' && computerSelection === 'ROCK'){
+
+        let result = "No one wins! Rock and Rock are tie!";
+        return result;
+
+    }else if (playerSelection === 'ROCK' && computerSelection === 'SCISSORS'){
+        
+        let result = "You won! Rock beats Scissors!"
+        return result;
+    }
+
+
+}
+
+const playerSelection = 'rock';
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection,computerSelection));
+
